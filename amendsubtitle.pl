c793cd7d9ddf5220a7cb5c30a07a5823215822ad
Plato Wu <gtalk000@gmail.com>
2012-12-20 22:17:33 +0800

    increase offset command line parameter. user 1970/1/1 for timelocal

--- amendsubtitle.pl
+++ amendsubtitle.pl
@@ -1,10 +1,13 @@
-# usage: perl amendsubtitle.pl in.srt > out.srt
+# usage: perl amendsubtitle.pl in.srt offset > out.srt
 
 use Time::Local;
+
+open(input,"$ARGV[0]") or die $!;
+
 my $flag = 1;
-while (<>) {
+while (<input>) {
        # Plato Wu,2009/06/29: set subtitle's offset(negative offset) to it before run.
-       my $offset = -76;#3626; #2845;
+       my $offset = $ARGV[1];
        if($flag){      
        # Plato Wu,2009/06/29: it is time line
         print $_;
@@ -18,17 +21,18 @@ while (<>) {
 #        print "$array[0];$array[1];$array[2];$array[3];$array[4]";        
         my ($hours, $min, $sec) = split/:/, $array[0];  
 
-        my $time = timelocal($sec, $min, $hours,16,2,2008);
+        my $time = timelocal($sec, $min, $hours,1,1,1970);
         $time -= $offset;
         ($sec, $min, $hour) = localtime $time;
         print "$hour:$min:$sec,$array[1] $array[2] ";
         #print "$array[3]\n";
         ($hours,$min,$sec) = split /:/, $array[3];
-        #print "$hours;:$min:rint "aa $sec\n";
-        $time = timelocal($sec, $min, $hours,16,2,2008);        
+        #print "$hours;:$min:rint aa $sec\n";
+        $time = timelocal($sec, $min, $hours,1,1,1970);
         $time -= $offset;
         ($sec, $min, $hours) = localtime $time;
         print "$hours:$min:$sec,$array[4]";      
         $flag = 1;
       }
 }
+close(input);
